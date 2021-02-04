112bb7b30337e9874d25245b78405dd37eb99362
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-02-04 19:33:44 +0100

    Changes made to some pages, Project and Resume Updated

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-// const mainMenu = document.querySelector('.nav-item');
-// const closeMenu = document.querySelector('.closeMenu');
-// const openMenu = document.querySelector('.openMenu');
+const mainMenu = document.querySelector('.nav-item');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 const myName = 'Hi, I am Arimoro Olamilekan';
 const myProfile = 'A solution driven software developer';
 
-document.getElementById('autoWriter').innerText = myName;
-document.getElementById('autoWriter1').innerText = myProfile;
 
-// // Hamburger Menu
-// openMenu.addEventListener('click', show);
-// closeMenu.addEventListener('click', close);
+// Hamburger Menu
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
 
-// // Show Menu
-// function show() {
-//   mainMenu.style.display = 'flex';
-//   mainMenu.style.top = '0';
-// }
+// Show Menu
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
 
-// // Close Menu
-// function close() {
-//   mainMenu.style.top = '-1000px';
-// }
+// Close Menu
+function close() {
+  mainMenu.style.top = '-1000px';
+}
 
-// // Name Auto Writer
-// let index = 0;
+// Name Auto Writer
+let index = 0;
 
-// function autoWriter() {
-//   document.getElementById('autoWriter').innerText = myName.slice(0, index);
-//   document.getElementById('autoWriter1').innerText = myProfile.slice(0, index);
+function autoWriter() {
+  document.getElementById('autoWriter').innerText = myName.slice(0, index);
+  document.getElementById('autoWriter1').innerText = myProfile.slice(0, index);
 
-//   index++;
+  index++;
 
-//   if (index > myProfile.length) {
-//     index = 0;
-//   }
-// }
+  if (index > myProfile.length) {
+    index = 0;
+  }
+}
 
-// setInterval(() => {
-//   autoWriter();
-// }, 500);
+setInterval(() => {
+  autoWriter();
+}, 500);
